bda9b02a252b824b24ae692ba9fa2d54fd9cb598
daveclinton <clintondavid46@gmail.com>
2024-04-13 11:01:01 +0300

    feat: Add chart financial summary  component

---
+++ app/components/charts/financialsummary.js
@@ -0,0 +1,211 @@
+"use client";
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
+const options = {
+  legend: {
+    show: false,
+    position: "top",
+    horizontalAlign: "left",
+  },
+  colors: ["#007B99", "#DB1471"],
+  chart: {
+    fontFamily: "Satoshi, sans-serif",
+    height: 335,
+    type: "area",
+    dropShadow: {
+      enabled: true,
+      color: "#fff",
+      top: 10,
+      blur: 4,
+      left: 0,
+      opacity: 0.1,
+    },
+
+    toolbar: {
+      show: false,
+    },
+  },
+  responsive: [
+    {
+      breakpoint: 1024,
+      options: {
+        chart: {
+          height: 300,
+        },
+      },
+    },
+    {
+      breakpoint: 1366,
+      options: {
+        chart: {
+          height: 350,
+        },
+      },
+    },
+  ],
+  stroke: {
+    width: [2, 2],
+    curve: "straight",
+  },
+  grid: {
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "category",
+    categories: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    title: {
+      style: {
+        fontSize: "0px",
+      },
+    },
+    min: 0,
+    max: 100,
+  },
+};
+
+const FinancialSummary = () => {
+  const [state, setState] = useState({
+    series: [
+      {
+        name: "Product One",
+        data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
+        fill: {
+          type: "gradient",
+          gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.0,
+            stops: [0, 100],
+            colorStops: [
+              {
+                offset: 0,
+                color: "#3C50E0",
+                opacity: 1,
+              },
+              {
+                offset: 100,
+                color: "#fff",
+                opacity: 1,
+              },
+            ],
+          },
+        },
+      },
+
+      {
+        name: "Product Two",
+        data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
+        fill: {
+          type: "gradient",
+          gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.0,
+            stops: [0, 100],
+            colorStops: [
+              {
+                offset: 0,
+                color: "#DB1471", // Start color for linear gradient
+                opacity: 1,
+              },
+              {
+                offset: 100,
+                color: "#fff", // End color for linear gradient
+                opacity: 1,
+              },
+            ],
+          },
+        },
+      },
+    ],
+  });
+
+  const handleReset = () => {
+    setState((prevState) => ({
+      ...prevState,
+    }));
+  };
+  handleReset;
+
+  return (
+    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5  sm:px-7.5 xl:col-span-8">
+      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
+        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
+          <div className="flex min-w-47.5">
+            <span className="mr-3 mt-1 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
+              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
+            </span>
+            <p className="font-semibold text-primary">Revenue</p>
+          </div>
+          <div className="flex min-w-47.5">
+            <span className="mr-3 mt-1 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-secondary">
+              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-secondary"></span>
+            </span>
+            <p className="font-semibold text-secondary">Expenses</p>
+          </div>
+        </div>
+        <div className="flex w-full max-w-45 justify-end">
+          <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
+            <button className="rounded bg-white px-3 py-1 text-xs font-medium text-black shadow-card hover:bg-white hover:shadow-card dark:bg-boxdark dark:text-white dark:hover:bg-boxdark">
+              Day
+            </button>
+            <button className="rounded px-3 py-1 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
+              Week
+            </button>
+            <button className="rounded px-3 py-1 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
+              Month
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div id="chartOne" className="-ml-5">
+          <ReactApexChart
+            options={options}
+            series={state.series}
+            type="area"
+            height={350}
+            width={"100%"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FinancialSummary;
--- app/user/[slug]/dashboard/page.js
+++ app/user/[slug]/dashboard/page.js
@@ -1,8 +1,11 @@
-import Overview from "@/app/components/features/userAccount/dashboard/overview"
+import Overview from "@/app/components/features/userAccount/dashboard/overview";
+import Overview from "@/app/components/features/dashboard/overview";
+import FinancialSummary from "@/app/components/charts/financialsummary";
 export default function DashboardPage() {
-    return (
-        <div className="w-full flex flex-col gap-5">
-           <Overview />
-        </div>
-    )
+  return (
+    <div className="w-full flex flex-col gap-5">
+      <Overview />
+      <FinancialSummary />
+    </div>
+  );
 }
