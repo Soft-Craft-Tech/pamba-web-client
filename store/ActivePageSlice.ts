bd42390b106854618d10159f11c4e105021116f0
daveclinton <clintondavid46@gmail.com>
2024-04-17 23:07:42 +0300

    feat: add react query

---
+++ store/ActivePageSlice.ts
@@ -0,0 +1,26 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// import type { PayloadAction } from "@reduxjs/toolkit";
+// import { RootState } from "./store";
+
+// // Define a type for the slice state
+// interface PageState {
+//   value: string;
+// }
+
+// // Define the initial state using that type
+// const initialState: PageState = {
+//   value: "home",
+// };
+
+// export const counterSlice = createSlice({
+//   name: "counter",
+//   initialState,
+//   reducers: {},
+// });
+
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// // Other code such as selectors can use the imported `RootState` type
+// export const selectCount = (state: RootState) => state.counter.value;
+
+// export default counterSlice.reducer;
