753940432e22fb08992af95614e8386014b61548
daveclinton <clintondavid46@gmail.com>
2024-04-17 21:54:59 +0300

    feat: add children type

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -1,12 +1,14 @@
+import { ReactNode } from "react";
+
 export const metadata = {
-    title: "Pamba Services",
-    description: "Simplify Your Operations With Effortless Business Management",
+  title: "Pamba Services",
+  description: "Simplify Your Operations With Effortless Business Management",
 };
 
-export default function AllServicesLayout({children}) {
-    return (
-        <>
-           {children} 
-        </>
-    )
+export default function AllServicesLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <>{children}</>;
 }
