720f6c7350f5aaedee2ae4030bbf05703973bf41
daveclinton <clintondavid46@gmail.com>
2024-04-23 10:39:01 +0300

    feat: add correct params type

---
+++ app/request-password-reset/page.tsx
@@ -0,0 +1,9 @@
+import RequestResetForm from "@/components/forms/requestReset";
+
+export default function RequestResetPages() {
+  return (
+    <div className="">
+      <RequestResetForm />
+    </div>
+  );
+}
