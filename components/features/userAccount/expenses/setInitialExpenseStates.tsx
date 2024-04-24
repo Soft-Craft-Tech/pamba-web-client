8ee71cdb369cd78bc84f00864a65429b830dbe6e
daveclinton <clintondavid46@gmail.com>
2024-04-24 10:18:27 +0300

    fix: remove unused code

--- components/features/userAccount/expenses/setInitialExpenseStates.tsx
+++ components/features/userAccount/expenses/setInitialExpenseStates.tsx
@@ -1,12 +1,3 @@
-"use client";
-import { useAppDispatch } from "@/hooks";
-import { setActivePage } from "@/store/sideHamburgerSlice";
-import { useEffect } from "react";
-
 export default function InitialExpenseStates() {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(setActivePage("Expenses"));
-  }, []);
   return <></>;
 }
