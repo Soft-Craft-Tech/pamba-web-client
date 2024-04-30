6e4408c120c64a88ca940032b41704651b2e6e54
daveclinton <clintondavid46@gmail.com>
2024-04-30 10:27:59 +0300

    fix: Fix locla storage undefime error

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,25 +1,39 @@
+"use client";
+
 import dayjs from "dayjs";
 
 export const getUser = () => {
-  const authUser = localStorage.getItem("authToken") || null;
+  const authUser =
+    (typeof window !== "undefined" &&
+      window.localStorage.getItem("authToken")) ||
+    null;
+
   return JSON.parse(String(authUser)) || {};
 };
 
 export const setUser = (value: any) => {
-  localStorage.setItem(
-    "authToken",
-    JSON.stringify({ ...value, expires: dayjs().add(1, "d") })
-  );
+  typeof window !== "undefined" &&
+    window.localStorage.setItem(
+      "authToken",
+      JSON.stringify({ ...value, expires: dayjs().add(1, "d") })
+    );
 };
 
 export const authUser = () => {
-  const authUser = window.localStorage.getItem("authToken") || null;
+  const authUser =
+    (typeof window !== "undefined" &&
+      window.localStorage.getItem("authToken")) ||
+    null;
   return authUser;
 };
 
-export const logoutUser = () => localStorage.removeItem("authToken");
+export const logoutUser = () =>
+  typeof window !== "undefined" && window.localStorage.removeItem("authToken");
 
 export const isAuthenticated = () => {
-  const authToken = localStorage.getItem("authToken") || null;
+  const authToken =
+    (typeof window !== "undefined" &&
+      window.localStorage.getItem("authToken")) ||
+    null;
   return authToken ? true : false;
 };
