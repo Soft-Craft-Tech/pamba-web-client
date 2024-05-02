5a65307cdab1e1892f2f6f6278cd043d074417cc
daveclinton <clintondavid46@gmail.com>
2024-05-02 15:09:25 +0300

    feat: add design

--- app/find-services/page.tsx
+++ app/find-services/page.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import RevampedHeader from "@/components/shared/header/revampedHeader";
 import ServiceHero from "@/components/service-hero";
 import { CategoryCard } from "@/components/core/cards/categoryCard";
+import Separator from "@/components/shared/sectionSeparators/separator";
 
 const FindServices: React.FC = () => {
   return (
@@ -9,20 +10,23 @@ const FindServices: React.FC = () => {
       <RevampedHeader />
       <ServiceHero />
       <div
-        className="mx-auto flex flex-col items-center justify-center py-32 max-w-screen-xl
+        className="mx-auto flex flex-col items-center justify-center py-16 max-w-screen-xl
          w-full"
       >
-        <div className="px-0.5 py-2 text-primary rounded-lg bg-gradient-to-r  md:from-[#EFE5EF]">
-          SERVICE CATEGORIES
-        </div>
-        <div className="w-full flex flex-wrap gap-6 justify-center lg:w-2/3">
-          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
-          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
-          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
-          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
-          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
-          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
-        </div>
+        <Separator
+          btnText={"SERVICE CATEGORIES"}
+          header={"Discover Your Ultimate Beauty and wellness Destination"}
+        />
+        <section className="pb-16 flex justify-center lg:px-20 lg:pb-16 w-full lg:w-2/3">
+          <div className="w-full flex flex-wrap gap-12 justify-evenly">
+            <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+            <CategoryCard img="/make-up.svg" text="Barbersalon" />
+            <CategoryCard img="/wedding-stylist.svg" text="Barbersalon" />
+            <CategoryCard img="/massage-spa.svg" text="Barbersalon" />
+            <CategoryCard img="/tattoo-parlor.svg" text="Barbersalon" />
+            <CategoryCard img="/makestar.svg" text="Barbersalon" />
+          </div>
+        </section>
       </div>
     </div>
   );
