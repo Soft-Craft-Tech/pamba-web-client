19ad75aff8d9f30e61c5116433d6e29fbcea5cf1
daveclinton <clintondavid46@gmail.com>
2024-05-02 13:28:15 +0300

    fix: Add ctageories

--- app/find-services/page.tsx
+++ app/find-services/page.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import RevampedHeader from "@/components/shared/header/revampedHeader";
 import ServiceHero from "@/components/service-hero";
+import { CategoryCard } from "@/components/core/cards/categoryCard";
 
 const FindServices: React.FC = () => {
   return (
     <div>
       <RevampedHeader />
       <ServiceHero />
+      <div
+        className="mx-auto flex flex-col items-center justify-center py-32 max-w-screen-xl
+         w-full"
+      >
+        <div className="px-0.5 py-2 text-primary rounded-lg bg-gradient-to-r  md:from-[#EFE5EF]">
+          SERVICE CATEGORIES
+        </div>
+        <div className="w-full flex flex-wrap gap-6 justify-center lg:w-2/3">
+          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+          <CategoryCard img="/barberSalon.svg" text="Barbersalon" />
+        </div>
+      </div>
     </div>
   );
 };
--- components/core/cards/categoryCard.tsx
+++ components/core/cards/categoryCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-export default function Category({ img, text }: { img: string; text: string }) {
+export function Category({ img, text }: { img: string; text: string }) {
   return (
     <div className="w-28 h-28 flex flex-col gap-2 bg-white p-3 rounded-lg items-center justify-center">
       <Image
@@ -13,3 +13,23 @@ export default function Category({ img, text }: { img: string; text: string }) {
     </div>
   );
 }
+
+export const CategoryCard: React.FC<{ text: string; img: string }> = ({
+  text,
+  img,
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-28 h-28  rounded-full items-center justify-center">
+        <Image
+          className="w-full h-full"
+          src={img}
+          alt="pamba-icons"
+          width={24}
+          height={24}
+        />
+      </div>
+      <h2 className="font-bold text-secondary text-sm text-center">{text}</h2>
+    </div>
+  );
+};
