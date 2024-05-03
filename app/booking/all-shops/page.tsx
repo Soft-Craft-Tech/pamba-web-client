a5435f822dcde28742f412c27a4395a3add021c7
daveclinton <clintondavid46@gmail.com>
2024-05-03 05:49:07 +0300

    feat: add cards

--- app/booking/all-shops/page.tsx
+++ app/booking/all-shops/page.tsx
@@ -7,11 +7,15 @@ const AllShops = () => {
   return (
     <div>
       <AllShopsHero />
-      <div className="w-full h-auto p-5 flex flex-col  gap-5 lg:gap-10 lg:p-20">
-        <h2 className=" text-[#0F1C35] text-2xl  sm:text-4xl">Popular Shops</h2>
+      <div className="w-full h-auto p-5 flex flex-col justify-center items-center   gap-5 lg:gap-10 lg:p-10">
+        <div className=" 3xl:max-w-[80%] lg:px-20  w-full">
+          <h2 className=" text-[#0F1C35] text-2xl sm:text-4xl">
+            Popular Shops
+          </h2>
+        </div>
       </div>
       <section className="pb-16 flex justify-center lg:px-20 lg:pb-16 w-full ">
-        <div className="w-full flex flex-wrap justify-center gap-12 center ">
+        <div className="w-full flex flex-wrap justify-center gap-12 3xl:max-w-[80%] ">
           {sliderDataTwo?.map(({ imageUrl, shopName }, index) => (
             <Explorer key={index} imageUrl={imageUrl} shopName={shopName} />
           ))}
