1c85bf330992d21f018c7fc3860762484fa157da
regan-mu <regansomi@gmail.com>
2024-05-08 01:40:15 +0300

    Feat: Login password eye toggle

--- components/forms/loginForm.tsx
+++ components/forms/loginForm.tsx
@@ -10,6 +10,7 @@ import Toast from "../shared/toasts/authToast";
 import React from "react";
 import { setShowToast } from "@/store/toastSlice";
 import { AxiosError } from "axios";
+import Image from "next/image";
 
 type LoginFormInputs = {
   username: string;
@@ -31,6 +32,7 @@ export default function LoginForm() {
   const [message, setMessage] = React.useState("");
   const [error, setError] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
 
   const onSubmit: SubmitHandler<LoginFormInputs> = async ({
     username,
@@ -73,7 +75,7 @@ export default function LoginForm() {
           control={control}
           render={({ field }) => (
             <input
-              className="w-full h-14 rounded-md border border-borders px-2 py-1 lg:h-12 xl:h-14"
+              className={`w-full h-14 rounded-md  border px-2 py-1 outline-none lg:h-12 xl:h-14 ${errors.username ? 'border-red-500': 'border-borders'}`}
               type="text"
               {...field}
               placeholder="Email"
@@ -81,21 +83,45 @@ export default function LoginForm() {
           )}
           rules={{ required: true }}
         />
-        {errors.username && <span>This field is required</span>}
-        <Controller
-          name="password"
-          control={control}
-          render={({ field }) => (
-            <input
-              className="w-full h-14 rounded-md border border-borders px-2 py-1 lg:h-12 xl:h-14"
-              type="password"
-              {...field}
-              placeholder="Password"
-            />
-          )}
-          rules={{ required: true }}
-        />
-        {errors.password && <span>This field is required</span>}
+        {errors.username && <span className="text-red-500 font-light text-xs">This field is required</span>}
+        <div className={`w-full h-14 relative rounded-md border lg:h-12 xl:h-14 ${errors.username ? 'border-red-500' : 'border-borders'}`}>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <input
+                className="h-full w-full  py-1 px-2 outline-none rounded-md"
+                type={showPassword ? "text" : "password"}
+                {...field}
+                placeholder="Password"
+              />
+            )}
+            rules={{ required: true }}
+          />
+          {errors.password && <span className="text-red-500 font-light text-xs">This field is required</span>}
+          <div
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+              className="absolute flex items-center h-full w-max top-0 right-1 px-2 hover:text-gray-300"
+            >
+              {showPassword ? (
+                <Image
+                  src="/eye-open.png"
+                  alt="Toggle password visibility"
+                  width={24}
+                  height={24}
+                />
+              ) : (
+                <Image
+                  src="/eye-closed.png"
+                  alt="Toggle password visibility"
+                  width={24}
+                  height={24}
+                />
+              )}
+          </div>
+        </div>
         <div className="flex justify-end">
           <Link
             href="/request-password-reset"
