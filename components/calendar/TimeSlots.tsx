815c346d8797240840fb07b329582605b615e0c3
daveclinton <clintondavid46@gmail.com>
2024-05-13 07:40:59 +0300

    feat: add loader

--- components/calendar/TimeSlots.tsx
+++ components/calendar/TimeSlots.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Fragment, useRef } from "react";
+import { Fragment, useRef, useState, useEffect } from "react";
 import { Scheduler } from "@aldabil/react-scheduler";
 import { useGetEvents } from "@/app/api/requests";
 import { SchedulerRef } from "@aldabil/react-scheduler/types";
@@ -8,27 +8,30 @@ import React from "react";
 const TimeSlots: React.FC = () => {
   const calendarRef = useRef<SchedulerRef>(null);
   const { data, isSuccess } = useGetEvents();
+  const [events, setEvents] = useState<any[]>([]);
 
-  const convertedAppointments: any = isSuccess
-    ? data.appointments.map((appointment: any) => ({
-        ...appointment,
-        start: new Date(appointment.start),
-        end: new Date(appointment.end),
-      }))
-    : [];
+  useEffect(() => {
+    if (isSuccess) {
+      const convertedAppointments = data.appointments.map(
+        (appointment: any) => ({
+          ...appointment,
+          start: new Date(appointment.start),
+          end: new Date(appointment.end),
+        })
+      );
+      setEvents(convertedAppointments);
+    }
+  }, [data, isSuccess]);
 
   const fetchRemote = async () => {
-    const data = await convertedAppointments;
-    return data;
+    return events;
   };
 
   return (
     <Fragment>
-      <Scheduler
-        ref={calendarRef}
-        getRemoteEvents={fetchRemote}
-        deletable={false}
-      />
+      {isSuccess && (
+        <Scheduler ref={calendarRef} deletable={false} events={events} />
+      )}
     </Fragment>
   );
 };
