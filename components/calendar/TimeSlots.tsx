6436a2f5960f2e03f0b4a0e9247ce59c2ee7a0d9
daveclinton <clintondavid46@gmail.com>
2024-05-22 12:39:32 +0300

    fix: Fix scheduler time display

--- components/calendar/TimeSlots.tsx
+++ components/calendar/TimeSlots.tsx
@@ -71,6 +71,16 @@ const TimeSlots: React.FC = () => {
           deletable={false}
           view="month"
           events={events}
+          editable={false}
+          hourFormat="24"
+          week={{
+            startHour: 7,
+            endHour: 23,
+            step: 50,
+            weekDays: [0, 1, 2, 3, 4, 5],
+            weekStartOn: 6,
+          }}
+          day={{ startHour: 7, endHour: 23, step: 50 }}
         />
       )}
     </Fragment>
