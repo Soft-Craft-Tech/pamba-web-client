5afb25cc3450e2c16e3e1a5200d7167f0293d93d
sonylomo <sonylomo1@gmail.com>
2024-07-05 11:51:45 +0300

    fix: Expense graph display, form validation, create and edit btns

--- components/charts/expenseSummary.tsx
+++ components/charts/expenseSummary.tsx
@@ -1,116 +1,114 @@
 "use client";
-import { useGetExpenses } from "@/app/api/expenses";
+import { useGetBusinessesAnalysis } from "@/app/api/businesses";
 import { ApexOptions } from "apexcharts";
+import dayjs from "dayjs";
+import { ExpensesType } from "../types";
 import ChartSummary from "./chartSummary";
 
-const options: ApexOptions = {
-  legend: {
-    show: false,
-    position: "top",
-    horizontalAlign: "left",
-  },
-  colors: ["#DB1471", "#007B99"],
-  chart: {
-    fontFamily: "Satoshi, sans-serif",
-    height: 335,
-    type: "area",
-    dropShadow: {
-      enabled: true,
-      color: "#fff",
-      top: 10,
-      blur: 4,
-      left: 0,
-      opacity: 0.1,
-    },
+const ExpenseSummary = () => {
+  const { data } = useGetBusinessesAnalysis();
 
-    toolbar: {
+  const expenseAmounts = data?.lifetime_expenses?.map(
+    (expense: ExpensesType) => expense.amount
+  );
+
+  const expenseMonths = data?.lifetime_expenses?.map(
+    (expense: ExpensesType) => {
+      return dayjs(expense.created_at).format("MMM");
+    }
+  );
+
+  const options: ApexOptions = {
+    legend: {
       show: false,
+      position: "top",
+      horizontalAlign: "left",
     },
-  },
-  responsive: [
-    {
-      breakpoint: 1024,
-      options: {
-        chart: {
-          height: 300,
-        },
+    colors: ["#DB1471", "#007B99"],
+    chart: {
+      fontFamily: "Satoshi, sans-serif",
+      height: 335,
+      type: "area",
+      dropShadow: {
+        enabled: true,
+        color: "#fff",
+        top: 10,
+        blur: 4,
+        left: 0,
+        opacity: 0.1,
       },
-    },
-    {
-      breakpoint: 1366,
-      options: {
-        chart: {
-          height: 350,
-        },
-      },
-    },
-  ],
-  stroke: {
-    width: [2, 2],
-    curve: "straight",
-  },
-  grid: {
-    xaxis: {
-      lines: {
+
+      toolbar: {
         show: false,
+      },
+    },
+    responsive: [
+      {
+        breakpoint: 1024,
+        options: {
+          chart: {
+            height: 300,
+          },
+        },
+      },
+      {
+        breakpoint: 1366,
+        options: {
+          chart: {
+            height: 350,
+          },
+        },
+      },
+    ],
+    stroke: {
+      width: [2, 2],
+      curve: "straight",
+    },
+    grid: {
+      xaxis: {
+        lines: {
+          show: false,
+        },
+      },
+      yaxis: {
+        lines: {
+          show: false,
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      type: "category",
+      categories: expenseMonths,
+      axisBorder: {
+        show: true,
+      },
+      axisTicks: {
+        show: true,
       },
     },
     yaxis: {
-      lines: {
-        show: false,
+      title: {
+        style: {
+          fontSize: "0px",
+        },
       },
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  xaxis: {
-    type: "category",
-    categories: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-    axisBorder: {
-      show: true,
-    },
-    axisTicks: {
-      show: true,
-    },
-  },
-  yaxis: {
-    title: {
-      style: {
-        fontSize: "0px",
+      axisBorder: {
+        show: true,
       },
+      axisTicks: {
+        show: true,
+      },
+      min: 0,
     },
-    axisBorder: {
-      show: true,
-    },
-    axisTicks: {
-      show: true,
-    },
-    min: 0,
-    max: 100,
-  },
-};
-
-const ExpenseSummary = () => {
-  const { data } = useGetExpenses();
+  };
 
   const series = [
     {
       name: "Expenses",
-      data,
+      data: expenseAmounts,
       fill: {
         type: "gradient",
         gradient: {
