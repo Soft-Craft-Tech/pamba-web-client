1be2f7fbcd71759689612a091a0446298876acf6
sonylomo <sonylomo1@gmail.com>
2024-07-23 01:38:59 +0300

    fix: add client form

--- components/tables/clientsTable.tsx
+++ components/tables/clientsTable.tsx
@@ -1,14 +1,18 @@
 "use client";
+import { useBookAppointments } from "@/app/api/appointment";
 import { useGetAllServices } from "@/app/api/businesses";
-import { useCreateClients, useGetAllClients } from "@/app/api/clients";
+import { useGetAllClients } from "@/app/api/clients";
 import Button from "@/ui/button";
 import FormField from "@/ui/FormField";
+import ReactSelectComponent from "@/ui/Select";
 import { getUser } from "@/utils/auth";
 import { clientSchema } from "@/utils/zodSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import FormControl from "@mui/material/FormControl";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateValidationError } from "@mui/x-date-pickers/models";
+import dayjs from "dayjs";
 import {
   MaterialReactTable,
   useMaterialReactTable,
@@ -20,16 +24,7 @@ import { useMemo, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { FaPlus } from "react-icons/fa";
 import * as z from "zod";
-import {
-  BusinessServiceType,
-  ServiceInfoType,
-  WebApppointmentBookingType,
-} from "../types";
-import { useBookAppointments } from "@/app/api/appointment";
-import { InputLabel } from "@mui/material";
-import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
+import { WebApppointmentBookingType } from "../types";
 
 type ClientsType = {
   id: number;
@@ -48,10 +43,12 @@ const ClientsTable = () => {
     handleSubmit,
     reset,
     register,
-    formState: { errors },
+    formState: { errors, isSubmitted },
   } = useForm<FormValues>({
     resolver: zodResolver(clientSchema),
   });
+
+  const todaysDate = new Date().toUTCString();
 
   const [globalFilter, setGlobalFilter] = useState("");
   const [sorting, setSorting] = useState<MRT_SortingState>([]);
@@ -59,23 +56,35 @@ const ClientsTable = () => {
     pageIndex: 0,
     pageSize: 10,
   });
-
+  const [selectedDate, setSelectedDate] = useState(null);
   const { client } = getUser();
   const { data, isPending, isError } = useGetAllClients();
   const { mutateAsync, status: createClientStatus } = useBookAppointments();
   const { data: allServices } = useGetAllServices(client?.slug);
+  const [dateError, setDateError] = useState<DateValidationError | null>(null);
+
+  const errorMessage = useMemo(() => {
+    switch (dateError) {
+      case "invalidDate": {
+        return "Your date is not valid";
+      }
+      default: {
+        return "";
+      }
+    }
+  }, [dateError]);
 
   const submitClient = async (formData: FormValues) => {
     const now = dayjs().format("HH:mm");
 
     const data: WebApppointmentBookingType = {
       name: formData.name,
-      date: dayjs(formData.appointmentDate).format("DD-MM-YYYY"),
+      date: dayjs(selectedDate).format("DD-MM-YYYY"),
       time: now,
       comment: "",
       staff: "",
       business: client?.id,
-      service: formData.service,
+      service: formData.service.value,
       email: formData.email,
       phone: formData.phone,
       notification: "email",
@@ -152,13 +161,30 @@ const ClientsTable = () => {
             register={register}
             error={errors.phone}
           />
-          <FormField
-            type="date"
-            placeholder="Appointment Date"
-            name="appointmentDate"
-            register={register}
-            error={errors.appointmentDate}
-          />
+     
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Appointment Date"
+              value={selectedDate}
+              onChange={(e) => {
+                let val = e as any;
+                setSelectedDate(val?.M?.$d);
+              }}
+              onError={(newError) => setDateError(newError)}
+            />
+          </LocalizationProvider>
+    
+          {(errorMessage !== "" && (
+            <span className="bg-red-100 text-red-700 p-4 rounded-lg">
+              {errorMessage}
+            </span>
+          )) ||
+            (selectedDate === null && isSubmitted && (
+              <span className="bg-red-100 text-red-700 p-4 rounded-lg">
+                Date is required!
+              </span>
+            ))}
+
           {/* <Controller
             control={control}
             name="appointmentTime.$d"
@@ -175,39 +201,27 @@ const ClientsTable = () => {
           )} */}
 
           <Controller
-            control={control}
             name="service"
+            control={control}
             render={({ field: { onChange, value } }) => (
-              <Select
-                value={value ?? ""}
+              <ReactSelectComponent
                 onChange={onChange}
-                inputProps={{ "aria-label": "Select service" }}
-                displayEmpty
-                renderValue={(selected) => {
-                  if (!selected) {
-                    return <em>Select Service</em>;
-                  }
-                  return (
-                    allServices.services.find(
-                      (service: BusinessServiceType) => service.id === selected
-                    )?.service || ""
-                  );
-                }}
-              >
-                {allServices &&
-                  allServices.services.map((service: BusinessServiceType) => (
-                    <MenuItem key={service?.id} value={service?.id}>
-                      {service?.service}
-                    </MenuItem>
-                  ))}
-              </Select>
+                options={
+                  allServices &&
+                  allServices.services.map(
+                    ({ service, id }: { service: string; id: number }) => ({
+                      value: id,
+                      label: service,
+                    })
+                  )
+                }
+                placeholder="Select Service"
+                value={value}
+                closeMenuOnSelect={true}
+                error={errors.service}
+              />
             )}
           />
-          {errors.service && (
-            <span className="bg-red-100 text-red-700 p-4 rounded-lg">
-              {errors.service.message}
-            </span>
-          )}
 
           <div className="flex h-auto w-full gap-5 justify-end mt-4">
             <button
