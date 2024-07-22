564ef31e8fa4dd1a91626b371fb4f07f0dcff2ce
sonylomo <sonylomo1@gmail.com>
2024-07-23 01:56:15 +0300

    refactor: redirect user to dashboard on profile completion

--- components/features/profileCompletion/completed.tsx
+++ components/features/profileCompletion/completed.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import Link from "next/link";
 
 export default function ProfileComplete() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/user/dashboard");
+  }, [router]);
+
   return (
     <div className="w-full h-full bg-background flex items-center justify-center">
       <div className="w-96 h-80 bg-white rounded-sm shadow-sm p-5 flex flex-col gap-5 justify-center items-center">
@@ -9,7 +18,10 @@ export default function ProfileComplete() {
         <p className="text-muted text-sm font-bold">
           You profile is now 100% complete
         </p>
-        <Link className="bg-primary text-white rounded-full py-2 px-5" href="/user/dashboard">
+        <Link
+          className="bg-primary text-white rounded-full py-2 px-5"
+          href="/user/dashboard"
+        >
           Go to Dashboard
         </Link>
       </div>
