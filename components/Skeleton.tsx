500b3b002f0fab6a2b08d0f770f35d006d5a3a4c
sonylomo <sonylomo1@gmail.com>
2024-12-17 12:43:23 +0300

    refactor: added social media links to footer

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -15,7 +15,7 @@ export default function SkeletonLoader() {
       <Stack spacing={1}>
         <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
 
-        <Skeleton variant="circular" width={40} height={40} />
+        {/* <Skeleton variant="circular" width={40} height={40} /> */}
         <Skeleton variant="rectangular" width={610} height={60} />
         <Skeleton variant="rectangular" width={610} height={60} />
       </Stack>
